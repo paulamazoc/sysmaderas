27af176c6eef874ef5be21fb2a0ee0351d3e7b76
Paula <paulamazoster@gmail.com>
2020-07-02 17:24:44 -0500

    Codebase: footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import './footer.css'
 
 class Footer extends React.Component {
     render () {
         return (
             <footer className="footer">
-                <p>Footer</p>
+                
+                <h1>Contáctanos</h1>
+                <p>Calle 47 No. 53 - 29</p>
+                <p>Itagüí, Antioquia</p>
+                
+                <p>277 07 50 | 281 36 38</p>
+                <p>316 446 15 71</p>
+                
+                <p>sysmaderas@gmail.com</p>
             </footer>
         );
     }
