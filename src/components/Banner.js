42b5cb6abf65b979ccc7e0d8b250c0849e2b2b72
LeSmgx <samg2583@hotmail.com>
2020-07-05 20:39:58 -0500

    Header - changes :3

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import './banner.css';
-import Header from './Header';
+import React from "react";
+import "./banner.css";
+import Header from "./Header";
 
 class Banner extends React.Component {
-    render () {
-        return (
-            <section class="banner">
-                <Header>{this.props.children}</Header>
-                <span className="banner-logo"></span>
-                <h2 className="banner-title">We are committed to simplicity in form.</h2>
-                <span className="banner-footer"></span>
-            </section>
-        );
-    }
+  render() {
+    return (
+      <section class="banner">
+        <Header>{this.props.children}</Header>
+        <span className="banner-logo"></span>
+        <div className="banner-title">
+          <h2>We are committed to simplicity in form.</h2>
+        </div>
+
+        <span className="banner-footer"></span>
+      </section>
+    );
+  }
 }
 
 export default Banner;
